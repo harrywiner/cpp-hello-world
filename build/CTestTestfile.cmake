ee112e202b15bf1e18605e22d08efc170c841f99
Harry Winer <winer.harry@gmail.com>
2023-07-22 13:39:29 +0100

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/beanboy/Chess/hello-world
+# Build directory: /home/beanboy/Chess/hello-world/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Boozer!!!!!!" << std::endl;
+    return 0;
+}
